300256c93be722e5a060be2797458d91771bee0c
hailuu <hoanghai.luu.71@gmail.com>
2023-07-26 00:04:02 +0700

    refactor code

--- Proxy-Server/Program.cs
+++ Proxy-Server/Program.cs
@@ -8,12 +8,10 @@ public class ProxyServer
 
     public ProxyServer()
     {
-        // Replace "localhost" with your Redis server address if needed
         var connectionOptions = ConfigurationOptions.Parse("localhost,abortConnect=false");
-        redisConnection = ConnectionMultiplexer.Connect(connectionOptions);
+        this.redisConnection = ConnectionMultiplexer.Connect(connectionOptions);
 
-        // Initialize HttpClient
-        httpClient = new HttpClient();
+        this.httpClient = new HttpClient();
     }
 
     public async Task StartProxyServer(string listenAddress, int listenPort)
@@ -27,7 +25,7 @@ public class ProxyServer
         while (true)
         {
             var context = await listener.GetContextAsync();
-            _ = HandleClientRequestAsync(context);
+            _ = this.HandleClientRequestAsync(context);
         }
     }
 
@@ -38,35 +36,30 @@ public class ProxyServer
 
         try
         {
-            // Check if the data exists in Redis (using request URL as cache key)
-            var cacheKey   = request.Url.ToString();
-            var cache      = redisConnection.GetDatabase();
+            var cacheKey   = request.Url!.ToString();
+            var cache      = this.redisConnection.GetDatabase();
             var cachedData = await cache.StringGetAsync(cacheKey);
 
             if (!cachedData.IsNull)
             {
-                // Serve the cached response to the client
-                response.ContentType = response.Headers["Content-Type"]; // Set the content type of the cached response
+                response.ContentType = response.Headers["Content-Type"];
                 response.ContentLength64 = cachedData.Length();
                 await response.OutputStream.WriteAsync(cachedData);
             }
             else
             {
-                // Fetch data from the destination server
-                var destinationUri     = new Uri("http://103.214.9.148"); // Replace with your destination server URL
+                var destinationUri     = new Uri("http://103.214.9.148");
                 var destinationRequest = new HttpRequestMessage(new HttpMethod(request.HttpMethod), destinationUri);
                 foreach (var headerKey in request.Headers.AllKeys)
                 {
                     destinationRequest.Headers.TryAddWithoutValidation(headerKey, request.Headers[headerKey]);
                 }
 
-                var destinationResponse = await httpClient.SendAsync(destinationRequest);
+                var destinationResponse = await this.httpClient.SendAsync(destinationRequest);
                 var responseData        = await destinationResponse.Content.ReadAsByteArrayAsync();
 
-                // Cache the response in Redis with an expiration time
                 await cache.StringSetAsync(cacheKey, responseData, expiry: TimeSpan.FromMinutes(5));
 
-                // Copy destination response headers to the proxy server response
                 foreach (var header in destinationResponse.Headers)
                 {
                     response.Headers[header.Key] = string.Join(",", header.Value);
@@ -74,7 +67,6 @@ public class ProxyServer
 
                 response.Headers["Content-Type"] = destinationResponse.Content.Headers.ContentType?.ToString();
 
-                // Serve the response back to the client
                 response.ContentType     = response.Headers["Content-Type"];
                 response.ContentLength64 = responseData.Length;
                 await response.OutputStream.WriteAsync(responseData);
